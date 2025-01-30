701f79f666067616686eead97d27fac3c5e91ce4
RoMANzhula <roman1985al@ukr.net>
2025-01-30 23:17:07 +0200

    Add BookService - getAllBooks() and change lombok's builder

--- src/main/java/org/romanzhula/book_api/services/BookService.java
+++ src/main/java/org/romanzhula/book_api/services/BookService.java
@@ -1,13 +1,17 @@
 package org.romanzhula.book_api.services;
 
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.book_api.controllers.requests.NewBookRequest;
+import org.romanzhula.book_api.controllers.responses.BookResponse;
+import org.romanzhula.book_api.mappers.BookMapper;
 import org.romanzhula.book_api.models.Book;
 import org.romanzhula.book_api.repositories.BookRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -15,20 +19,36 @@ import java.time.LocalDateTime;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     
     @Transactional
     public void createNewBookPosition(NewBookRequest bookRequest) {
 
-        Book newBook =  new Book().builder()
-                .title(bookRequest.getTitle())
-                .author(bookRequest.getAuthor())
-                .registrDate(LocalDateTime.now())
-                .publicationDate(bookRequest.getPublicationDate())
-                .build()
+        // with Lombok's annotation @Builder
+//        Book newBook =  new Book().builder()
+//                .title(bookRequest.getTitle())
+//                .author(bookRequest.getAuthor())
+//                .registrDate(LocalDateTime.now())
+//                .publicationDate(bookRequest.getPublicationDate())
+//                .build()
+//        ;
+
+        Book newBook =  new Book()
+                .setTitle(bookRequest.getTitle())
+                .setAuthor(bookRequest.getAuthor())
+                .setRegistrDate(LocalDateTime.now())
+                .setPublicationDate(bookRequest.getPublicationDate())
         ;
 
         bookRepository.save(newBook);
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookResponse> getAllBooks() {
+        List<Book> books = bookRepository.findAll();
+
+        return bookMapper.toBookResponseList(books);
     }
 
 }
