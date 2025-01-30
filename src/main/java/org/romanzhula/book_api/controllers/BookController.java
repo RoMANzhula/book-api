cd191aded97acd563b3bf774d85d86678cfa506e
RoMANzhula <roman1985al@ukr.net>
2025-01-30 23:14:29 +0200

    Up BookController - add getAllBooks() method

--- src/main/java/org/romanzhula/book_api/controllers/BookController.java
+++ src/main/java/org/romanzhula/book_api/controllers/BookController.java
@@ -2,12 +2,12 @@ package org.romanzhula.book_api.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.book_api.controllers.requests.NewBookRequest;
+import org.romanzhula.book_api.controllers.responses.BookResponse;
 import org.romanzhula.book_api.services.BookService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/books")
@@ -17,13 +17,20 @@ public class BookController {
     private final BookService bookService;
 
 
-    @PostMapping()
+    @PostMapping("/create-position")
     public ResponseEntity<String> createBook(
             @RequestBody NewBookRequest bookRequest
     ) {
         bookService.createNewBookPosition(bookRequest);
 
         return ResponseEntity.ok("This position created successfully!");
+    }
+
+    @GetMapping("/all-books")
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+        List<BookResponse> allBooks = bookService.getAllBooks();
+
+        return ResponseEntity.ok(allBooks);
     }
 
 }
