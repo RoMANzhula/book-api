edfdb842257c9068135e75c38d5805bf73fefd4a
RoMANzhula <roman1985al@ukr.net>
2025-01-30 23:11:54 +0200

    Up V1_0_0__create_table_book.sql - change id to UUID

--- src/main/resources/db/migration/V1_0_0__create_table_book.sql
+++ src/main/resources/db/migration/V1_0_0__create_table_book.sql
@@ -1,5 +1,5 @@
 CREATE TABLE book (
-    book_id VARCHAR(40) PRIMARY KEY NOT NULL,
+    book_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     publication_date DATE NOT NULL,
